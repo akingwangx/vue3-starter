32b0041a30b186992bfee5497965cf984a36f86a
wangxin <akingwangxin@gmail.com>
2021-07-15 15:27:53 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+    'plugin:prettier/recommended', // 添加 prettier 插件
+  ],
+  rules: {
+  },
+};
